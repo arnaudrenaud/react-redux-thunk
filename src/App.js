e555d84fd8710d5f353aa67f21b78ed99d4dd6a4
Arnaud Renaud <arnaud.renaud@gmail.com>
2019-01-15 12:18:34 +0100

    Set up app without Redux

--- src/App.js
+++ src/App.js
@@ -1,25 +1,55 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
+import React, { Component, Fragment } from 'react';
 import './App.css';
 
+import { ArticleList, Info } from './components';
+
 class App extends Component {
+  state = {
+    articles: [],
+    isLoading: true,
+  };
+
+  async componentDidMount() {
+    const response = await fetch(
+      'https://packing-list-weight-api.herokuapp.com/articles'
+    );
+    const articles = await response.json();
+    this.setState({
+      articles: articles.map(article => ({ ...article, isSelected: false })),
+      isLoading: false,
+    });
+  }
+
+  toggleArticleSelection = name => () => {
+    const { articles } = this.state;
+
+    this.setState({
+      articles: articles.map(article =>
+        article.name === name
+          ? { ...article, isSelected: !article.isSelected }
+          : article
+      ),
+    });
+  };
+
   render() {
+    const { articles, isLoading } = this.state;
+
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        {isLoading ? (
+          <div data-selector="App-isLoading" style={{ marginTop: '1em' }}>
+            Loading…
+          </div>
+        ) : (
+          <Fragment>
+            <ArticleList
+              articles={articles}
+              toggleArticleSelection={this.toggleArticleSelection}
+            />
+            <Info articles={articles} />
+          </Fragment>
+        )}
       </div>
     );
   }
