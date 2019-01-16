e9262b29dde8ef86e6f4e5b70803d8626485b24a
Arnaud Renaud <arnaud.renaud@gmail.com>
2019-01-16 17:35:48 +0100

    Configure Redux DevTools

--- src/StoreProvider.js
+++ src/StoreProvider.js
@@ -1,13 +1,17 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { Provider } from 'react-redux';
-import { applyMiddleware, createStore } from 'redux';
+import { applyMiddleware, compose, createStore } from 'redux';
 import thunk from 'redux-thunk';
 
 import rootReducer from './reducers';
 
+// eslint-disable-next-line
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 const StoreProvider = ({ children }) => (
-  <Provider store={createStore(rootReducer, applyMiddleware(thunk))}>
+  <Provider
+    store={createStore(rootReducer, composeEnhancers(applyMiddleware(thunk)))}
+  >
     {children}
   </Provider>
 );
